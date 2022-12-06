5750b95fd81e5e66fcb448cddcbacae64affcbf0
Alex L <95913221+Pwhsky@users.noreply.github.com>
2022-12-06 13:19:37 +0100

    Update 1.5.1.cpp

--- Subchapter1.5/1.5.1.cpp
+++ Subchapter1.5/1.5.1.cpp
@@ -3,31 +3,34 @@
 //Recursion, Geometric progression
 using namespace std;
 
-float geometric_progression(float first_step,float step_length, int n) {
 
-float summation = first_step;
-for (int i = 1; i<n;i++){
-summation = summation + first_step*pow(step_length,i);
-}
-    return summation;
-}
+float geometric_progression(float first_step,float step_length, int n) {
+    if (n==1)
+        return first_step;
+    else
+        return step_length*geometric_progression(first_step,step_length,n-1); 
+}cout<<"\n";
+cout<<"\n";
+system("pause");
 
 int main() {
 
-float a,r,n; 
+float a,q,n; 
 float result;
 
-cout<<"first step a: \n ";
+cout<<"enter first step a: \n ";
     cin>>a;
-cout<<"step-length r: \n ";
-    cin>>r;
+cout<<"enter step-length q: \n ";
+    cin>>q;
 cout<<"No. of steps n: \n";
     cin>>n; 
     
-    result = geometric_progression(a,r,n);
-cout<<"The series converges to: "<<result; 
+
+ result = geometric_progression(a,q,n);
+cout<<"The n:th element is: "<<result<<"\n";
+
 cout<<"\n";
 cout<<"\n";
 system("pause");
-return 0;
+    return 0;
     }
